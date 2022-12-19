65d5b13ed0f6699d90f058f4931a518d6d867fb0
Napatsutthitham <napatsutthitham@gmail.com>
2022-12-19 09:42:28 +0700

    updata

---
+++ week901.js
@@ -0,0 +1,20 @@
+function sevenE1ven(){
+      //lirt
+      const soil = "นํ้าปลา"
+      const price = 15
+      const unit = 2
+      const total= price * unit
+      console.log(total)
+
+}
+const sevenPrice = sevenE1ven()
+ function gobike(){
+    const motorcycle = 100
+    const mony = 100
+    const sum = mony + motorcycle
+    console.log(sum)
+    gobikePice = gobike()
+
+   
+ }
+
---
+++ week903.js
@@ -0,0 +1,8 @@
+//2.Pass varguments/parameter to function
+function add(){
+    const a = 15
+    const b = 10
+    console.log(a+b)
+    
+}
+add(15,10) //arguments
